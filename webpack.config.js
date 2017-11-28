fbf85b1dbed55940afc3b8acd4806c82b559cabb
iteratoruk <iteratoruk@gmail.com>
2017-11-28 19:06:22 +0000

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-	entry: './src/js/app.js',
+	entry: path.resolve(__dirname, 'src/js/app.js'),
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: "app.js"
-	}
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, 'dist')
+	},
+	plugins: [new HtmlWebpackPlugin({
+		template: path.resolve(__dirname, 'src/html/index.html')
+	})]
 };
 
 module.exports = config;
